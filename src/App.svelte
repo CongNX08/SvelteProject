e0083a0ae7ccb0e1a88e6805e2517380f47ce428
CongNX08 <congnx08@gmail.com>
2024-09-17 14:25:43 +0700

    init project Svelte

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	let name = 'Svelte';
+	let src = 'https://picsum.photos/200/300';
+	let string = 'Some <strong>Svelte</strong> with html ';
+</script>
+
+<img {src} alt="Some text {name}" />
+<h1>Hello {name.toLocaleLowerCase()}</h1>
+<p>{@html string}</p>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
