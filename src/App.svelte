4a73fb7924b6dd64081927b8122e39a7646640c4
CongNX08 <congnx08@gmail.com>
2024-09-17 14:35:36 +0700

    counter

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,8 @@
 <script>
-	let name = 'Svelte';
-	let src = 'https://picsum.photos/200/300';
-	let string = 'Some <strong>Svelte</strong> with html ';
+	import Counter from './lib/Counter.svelte';
 </script>
 
-<img {src} alt="Some text {name}" />
-<h1>Hello {name.toLocaleLowerCase()}</h1>
-<p>{@html string}</p>
+<Counter />
 
 <style>
-	h1 {
-		color: red;
-	}
 </style>
